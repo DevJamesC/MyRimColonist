6fa926b7a10472aadd94a8a13ab7f20257f5a099
James Crescenzi <jkc36@pitt.edu>
2024-03-31 22:37:46 -0500

    added a for loop for content generation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,23 @@ namespace WebApplication1.Controllers
 
         public IActionResult StartQuiz()
         {
+            ViewData["SubjectList"] = new RimworldSubjectData[]
+            {
+                new RimworldSubjectData("Shooting"),
+                new RimworldSubjectData("Melee"),
+                new RimworldSubjectData("Construction"),
+                new RimworldSubjectData("Mining"),
+                new RimworldSubjectData("Cooking"),
+                new RimworldSubjectData("Plants"),
+                new RimworldSubjectData("Animals"),
+                new RimworldSubjectData("Crafting"),
+                new RimworldSubjectData("Artisting"),
+                new RimworldSubjectData("Medical"),
+                new RimworldSubjectData("Social"),
+                new RimworldSubjectData("Intellectual"),
+
+            };
+
             return View("Quiz");
         }
         public IActionResult FinishQuiz()
@@ -36,6 +53,34 @@ namespace WebApplication1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class RimworldSubjectData
+    {
+        public string Name { get; set; } = "NA";
+        public int Level { get; set; } = 0;
+        public string Passion => PassionStringFromVal(PassionVal);
+        public int PassionVal { get; set; } = 1;
+
+        public RimworldSubjectData(string name)
+        {
+            Name = name;
+        }
+
+
+        private string PassionStringFromVal(int val)
+        {
+            string returnVal = "NA";
+            switch (val)
+            {
+                case 0: returnVal = "Apathy"; break;
+                case 1: returnVal = "Normal"; break;
+                case 2: returnVal = "Passon"; break;
+                case 3: returnVal = "Burning Passion"; break;
+                case 4: returnVal = "Critical Passion"; break;
+            }
+            return returnVal;
         }
     }
 }
