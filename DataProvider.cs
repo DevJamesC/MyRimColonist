e11ee9929c59fe25b0ba9674fe7f404094cfded2
James Crescenzi <jkc36@pitt.edu>
2024-04-16 22:49:51 -0500

    added sources and data structure for traits

---
+++ DataProvider.cs
@@ -0,0 +1,103 @@
+﻿using WebApplication1.Models;
+
+namespace MyRimColonist
+{
+    public static class DataProvider
+    {
+
+        public static string[] sources = new string[] { "Base", "BioTech", "Ideology", "Royalty", "Anomaly", "Vanilla Skills Expanded", "Vanilla Traits Expanded", "Vanilla Backgrounds Expanded" };
+
+        public static RimworldSubjectData[] GetSubjectData()
+        {
+            return new RimworldSubjectData[]
+            {
+                new RimworldSubjectData("Shooting"),
+                new RimworldSubjectData("Melee"),
+                new RimworldSubjectData("Construction"),
+                new RimworldSubjectData("Mining"),
+                new RimworldSubjectData("Cooking"),
+                new RimworldSubjectData("Plants"),
+                new RimworldSubjectData("Animals"),
+                new RimworldSubjectData("Crafting"),
+                new RimworldSubjectData("Artisting"),
+                new RimworldSubjectData("Medical"),
+                new RimworldSubjectData("Social"),
+                new RimworldSubjectData("Intellectual"),
+            };
+        }
+
+        public static RimworldBackstory[] GetTraitList()
+        {
+            return new RimworldBackstory[] {
+                new RimworldBackstory(){
+                    Name="Gourmand",
+                    Description="NAME's life revolves around food. S/He gets hungry quickly, and if s/he is in a bad mood, s/he will often satisfy her/his-self by eating.",
+                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Cooking") {Level=4 } },
+                    Source=sources[0]
+
+                },
+                new RimworldBackstory(){
+                    Name="Test 1",
+                    Description="Test trait 1",
+                    Source=sources[1]
+                },
+                new RimworldBackstory(){
+                    Name="Test 2",
+                    DisabledTasks=new string[]{ "Violent"},
+                    Description="Test 2- no violent",
+                    Source=sources[2]
+                },
+            };
+        }
+
+        public static RimworldBackstory[] GetChildBackstoryList()
+        {
+            return new RimworldBackstory[] {
+                new RimworldBackstory(){
+                    Name="Colony Child",
+                    Description="Born and raised in the colony",
+                    Source=sources[0]
+
+                },
+                new RimworldBackstory(){
+                    Name="Test 1",
+                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Shooting") {Level=2 }, new RimworldSubjectData("Mining") {Level=-1 } },
+                    Description="Test child backstory",
+                    Source=sources[1]
+                },
+                new RimworldBackstory(){
+                    Name="Test 2",
+                    DisabledTasks=new string[]{ "Violent", "Cooking"},
+                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Mining") {Level=5 }, new RimworldSubjectData("Social") {Level=-3 } },
+                    Description="Test child backstory- no violence or cooking",
+                    Source=sources[2]
+                },
+            };
+        }
+
+        public static RimworldBackstory[] GetAdultBackstoryList()
+        {
+            return new RimworldBackstory[] {
+                new RimworldBackstory(){
+                    Name="Colonist",
+                    Description="Lived in the colony all your life",
+                    Source=sources[0]
+                },
+                 new RimworldBackstory(){
+                    Name="Test 1",
+                    DisabledTasks=new string[]{"Social"},
+                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Medical") {Level=3 } },
+                    Description="Test adult backstory- no social",
+                    Source=sources[1]
+                },
+                 new RimworldBackstory(){
+                    Name="Test 2",
+                    DisabledTasks=new string[]{"Dumb Labor"},
+                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Artisting") {Level=3 },new RimworldSubjectData("Animals") {Level=2 },new RimworldSubjectData("Plants") {Level=-2 } },
+                    Description="Test adult backstory- no dumb labor",
+                    Source=sources[2]
+                },
+            };
+        }
+    }
+}
--- Models/RimStatsModel .cs
+++ Models/RimStatsModel .cs
@@ -1,3 +1,5 @@
+using MyRimColonist;
+
 namespace WebApplication1.Models
 {
     public class RimStatsModel
@@ -11,9 +13,9 @@ namespace WebApplication1.Models
         public int AdultBackstoryIndex { get; set; }
         public RimworldSubjectData[] Subjects { get; set; }
 
-
         public RimworldBackstory[] childhoodBackstoryList;
         public RimworldBackstory[] adultBackstoryList;
+        public RimworldBackstory[] TraitList;
         public RimStatsModel()
         {
             FirstName = "first";
@@ -26,58 +28,10 @@ namespace WebApplication1.Models
 
             //still need injuries and health conditions
 
-            childhoodBackstoryList = new RimworldBackstory[] {
-                new RimworldBackstory(){
-                    Name="Colony Child",
-                    Description="Born and raised in the colony"
-                },
-                new RimworldBackstory(){
-                    Name="Test 1",
-                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Shooting") {Level=2 }, new RimworldSubjectData("Mining") {Level=-1 } },
-                    Description="Test child backstory"
-                },
-                new RimworldBackstory(){
-                    Name="Test 2",
-                    DisabledTasks=new string[]{ "Violent", "Cooking"},
-                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Mining") {Level=5 }, new RimworldSubjectData("Social") {Level=-3 } },
-                    Description="Test child backstory- no violence or cooking"
-                },
-            };
-
-            adultBackstoryList = new RimworldBackstory[] {
-                new RimworldBackstory(){
-                    Name="Colonist",
-                    Description="Lived in the colony all your life"
-                },
-                 new RimworldBackstory(){
-                    Name="Test 1",
-                    DisabledTasks=new string[]{"Social"},
-                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Medical") {Level=3 } },
-                    Description="Test adult backstory- no social"
-                },
-                 new RimworldBackstory(){
-                    Name="Test 2",
-                    DisabledTasks=new string[]{"Dumb Labor"},
-                    SubjectStatChanges = new List<RimworldSubjectData>(){ new RimworldSubjectData("Artisting") {Level=3 },new RimworldSubjectData("Animals") {Level=2 },new RimworldSubjectData("Plants") {Level=-2 } },
-                    Description="Test adult backstory- no dumb labor"
-                },
-            };
-
-            Subjects = new RimworldSubjectData[]
-            {
-                new RimworldSubjectData("Shooting"),
-                new RimworldSubjectData("Melee"),
-                new RimworldSubjectData("Construction"),
-                new RimworldSubjectData("Mining"),
-                new RimworldSubjectData("Cooking"),
-                new RimworldSubjectData("Plants"),
-                new RimworldSubjectData("Animals"),
-                new RimworldSubjectData("Crafting"),
-                new RimworldSubjectData("Artisting"),
-                new RimworldSubjectData("Medical"),
-                new RimworldSubjectData("Social"),
-                new RimworldSubjectData("Intellectual"),
-            };
+            childhoodBackstoryList = DataProvider.GetChildBackstoryList();
+            adultBackstoryList = DataProvider.GetAdultBackstoryList();
+            TraitList = DataProvider.GetTraitList();
+            Subjects = DataProvider.GetSubjectData();
 
 
         }
@@ -153,6 +107,7 @@ namespace WebApplication1.Models
         public string Description { get; set; }
         public string[] DisabledTasks { get; set; }
         public List<RimworldSubjectData> SubjectStatChanges { get; set; }
+        public string Source { get; set; }
 
         public RimworldBackstory()
         {
@@ -160,6 +115,7 @@ namespace WebApplication1.Models
             Description = "";
             DisabledTasks = new string[0];
             SubjectStatChanges = new List<RimworldSubjectData>();
+            Source = "";
         }
     }
 }
